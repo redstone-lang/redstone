9f9bd133ca72c59045c98bf5efdbf76c3c3971f8
JKearnsl <boyarshinovn@gmail.com>
2026-04-11 13:25:03 +0300

    fix try_run

--- redstone/src/tests/mod.rs
+++ redstone/src/tests/mod.rs
@@ -15,13 +15,13 @@ fn try_build(src: &str) -> Result<(), String> {
 fn try_run(src: &str) -> Result<std::process::Command, String> {
     let tmp_file = NamedTempFile::new()
         .map_err(|e| format!("create temp file: {}", e))?;
-    let path = tmp_file.path().to_owned().to_str()
-        .ok_or("temp file path is not valid UTF-8".to_string())?
-        .to_string();
 
-    let _persisted = tmp_file
+    let (_, path_buf) = tmp_file
         .keep()
         .map_err(|e| format!("keep temp file: {}", e))?;
+    let path = path_buf.to_str()
+        .ok_or("temp file path is not valid UTF-8".to_string())?
+        .to_string();
 
     compile(src, CompileOptions {
         output: path.as_str(),
